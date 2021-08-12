620a7427e6216cbd91665db14f2629efb84e2d35
Erik Schnetter <schnetter@gmail.com>
2021-08-12 15:22:33 -0400

    Add MPI_Irecv

--- hello.c
+++ hello.c
@@ -1,45 +1,76 @@
 #include "mpi.h"
 
+#include <assert.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  fprintf(stderr, "main.0\n");
-
-  int initialized;
-  fprintf(stderr, "&MPI_Initialized=%p\n", &MPI_Initialized);
-  fprintf(stderr, "MPI_Initialized=%p\n", MPI_Initialized);
+  int initialized, finalized;
   MPI_Initialized(&initialized);
-  printf("initialized: %d\n", initialized);
+  assert(!initialized);
+  MPI_Finalized(&finalized);
+  assert(!finalized);
 
   MPI_Init(&argc, &argv);
-  fprintf(stderr, "main.1\n");
+
+  MPI_Initialized(&initialized);
+  assert(initialized);
+  MPI_Finalized(&finalized);
+  assert(!finalized);
 
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  fprintf(stderr, "main.2\n");
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  fprintf(stderr, "main.3\n");
   printf("size: %d, rank: %d\n", size, rank);
 
-  int isend = 42;
-  int irecv = -1;
-  MPI_Send(&isend, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
-  fprintf(stderr, "main.4\n");
-  MPI_Status status;
-  MPI_Recv(&irecv, 1, MPI_INT, (rank + size - 1) % size, 0, MPI_COMM_WORLD,
-           &status);
-  fprintf(stderr, "main.5\n");
-  int count;
-  MPI_Get_count(&status, MPI_INT, &count);
-  fprintf(stderr, "main.6\n");
-  if (rank == 0) {
-    printf("sent: %d, received: %d\n", isend, irecv);
-    printf("source: %d, tag: %d, error: %d, count: %d\n", status.MPI_SOURCE,
-           status.MPI_TAG, status.MPI_ERROR, count);
+  {
+    int isend = 42;
+    int irecv = -1;
+    MPI_Send(&isend, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
+    MPI_Status status;
+    MPI_Recv(&irecv, 1, MPI_INT, (rank + size - 1) % size, 0, MPI_COMM_WORLD,
+             &status);
+    assert(isend == 42);
+    assert(irecv == 42);
+    assert(status.MPI_SOURCE == (rank + size - 1) % size);
+    assert(status.MPI_TAG == 0);
+    int count;
+    MPI_Get_count(&status, MPI_INT, &count);
+    assert(count == 1);
+    if (rank == 0) {
+      printf("sent: %d, received: %d\n", isend, irecv);
+      printf("source: %d, tag: %d, error: %d, count: %d\n", status.MPI_SOURCE,
+             status.MPI_TAG, status.MPI_ERROR, count);
+    }
   }
 
+  {
+    int isend = 42;
+    int irecv = -1;
+    MPI_Request sreq;
+    MPI_Isend(&isend, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD, &sreq);
+    MPI_Request rreq;
+    MPI_Irecv(&irecv, 1, MPI_INT, (rank + size - 1) % size, 0, MPI_COMM_WORLD,
+              &rreq);
+    MPI_Wait(&sreq, MPI_STATUS_IGNORE);
+    MPI_Status status;
+    MPI_Wait(&rreq, &status);
+    assert(isend == 42);
+    assert(irecv == 42);
+    assert(status.MPI_SOURCE == (rank + size - 1) % size);
+    assert(status.MPI_TAG == 0);
+    int count;
+    MPI_Get_count(&status, MPI_INT, &count);
+    assert(count == 1);
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
   MPI_Finalize();
-  fprintf(stderr, "main.9\n");
+
+  MPI_Initialized(&initialized);
+  assert(initialized);
+  MPI_Finalized(&finalized);
+  assert(finalized);
 
   return 0;
 }
