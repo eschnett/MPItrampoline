9ed19935e1c7818b94529644ee3e3a274b59f195
Erik Schnetter <schnetter@gmail.com>
2021-08-25 20:07:23 -0400

    Switch back to dlopen

--- mpi.c
+++ mpi.c
@@ -116,9 +116,9 @@ init_mpitrampoline() {
 #ifdef __APPLE__
   void *handle = dlopen(libname, RTLD_LAZY | RTLD_LOCAL | RTLD_FIRST);
 #else
-  // void *handle = dlopen(libname, RTLD_LAZY | RTLD_LOCAL | RTLD_DEEPBIND);
-  // void *handle = dlmopen(LM_ID_NEWLM, libname, RTLD_LAZY);
-  void *handle = dlmopen(LM_ID_NEWLM, libname, RTLD_NOW);
+  void *handle = dlopen(libname, RTLD_LAZY | RTLD_LOCAL | RTLD_DEEPBIND);
+  //TODO void *handle = dlmopen(LM_ID_NEWLM, libname, RTLD_LAZY);
+  //TODO void *handle = dlmopen(LM_ID_NEWLM, libname, RTLD_NOW);
 #endif
   if (!handle) {
     fprintf(stderr, "Could not dlopen MPI wrapper library \"%s\"\n", libname);
