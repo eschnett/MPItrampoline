7e205d57bfdd5820d35d337a4f1eeff69e472eb0
Erik Schnetter <schnetter@gmail.com>
2021-09-08 13:23:24 -0400

    Implement MPITRAMPOLINE_PRELOAD

--- mpi.c
+++ mpi.c
@@ -46,6 +46,143 @@ extern inline int MPI_Pcontrol(int level, ...);
 
 ////////////////////////////////////////////////////////////////////////////////
 
+static bool verbose = false;
+
+static void set_verbose() { verbose = getenv("MPITRAMPOLINE_VERBOSE"); }
+
+enum dlopen_mode_t {
+  dlopen_mode_error,
+  dlopen_mode_dlmopen,
+  dlopen_mode_dlopen
+};
+static enum dlopen_mode_t dlopen_mode = dlopen_mode_error;
+
+void set_dlopen_mode() {
+  const char *const dlopen_mode_str = getenv("MPITRAMPOLINE_DLOPEN_MODE");
+  if (dlopen_mode_str == NULL || strcmp(dlopen_mode_str, "dlmopen") == 0) {
+    dlopen_mode = dlopen_mode_dlmopen;
+  } else if (strcmp(dlopen_mode_str, "dlopen") == 0) {
+    dlopen_mode = dlopen_mode_dlopen;
+  } else {
+    fprintf(
+        stderr,
+        "The environment variable MPITRAMPOLINE_DLOPEN_MODE is set to \"%s\".\n"
+        "Only the values \"dlmopen\" (default) and \"dlopen\" are allowed.",
+        dlopen_mode_str);
+    exit(1);
+  }
+}
+
+enum dlopen_binding_t {
+  dlopen_binding_error,
+  dlopen_binding_lazy,
+  dlopen_binding_now
+};
+static enum dlopen_binding_t dlopen_binding = dlopen_binding_error;
+
+void set_dlopen_binding() {
+  const char *const dlopen_binding_str = getenv("MPITRAMPOLINE_DLOPEN_BINDING");
+  if (dlopen_binding_str == NULL || strcmp(dlopen_binding_str, "lazy") == 0) {
+    dlopen_binding = dlopen_binding_lazy;
+  } else if (strcmp(dlopen_binding_str, "now") == 0) {
+    dlopen_binding = dlopen_binding_now;
+  } else {
+    fprintf(stderr,
+            "The environment variable MPITRAMPOLINE_DLOPEN_BINDING is set to "
+            "\"%s\".\n"
+            "Only the values \"lazy\" (default) and \"now\" are allowed.",
+            dlopen_binding_str);
+    exit(1);
+  }
+}
+
+static int get_dlopen_flags() {
+  int dlopen_flags;
+  switch (dlopen_binding) {
+  case dlopen_binding_lazy:
+    if (verbose)
+      fprintf(stderr, "[MPItrampoline] Using RTLD_LAZY\n");
+    dlopen_flags = RTLD_LAZY;
+    break;
+  case dlopen_binding_now:
+    if (verbose)
+      fprintf(stderr, "[MPItrampoline] Using RTLD_NOW\n");
+    dlopen_flags = RTLD_NOW;
+    break;
+  default:
+    assert(0);
+  }
+  return dlopen_flags;
+}
+
+#ifdef __linux__
+static bool have_dlopen_lmid = false;
+static Lmid_t dlopen_lmid;
+#endif
+
+static void *load_library(const char *const libname) {
+  const int dlopen_flags = get_dlopen_flags();
+
+  void *handle = NULL;
+
+#ifdef __APPLE__
+
+  if (verbose)
+    fprintf(stderr, "[MPItrampoline] Calling dlopen\n");
+  handle = dlopen(libname, dlopen_flags | RTLD_LOCAL | RTLD_FIRST);
+
+#elif __linux__
+
+  switch (dlopen_mode) {
+  case dlopen_mode_dlmopen:
+    if (verbose)
+      fprintf(stderr, "[MPItrampoline] Calling dlmopen\n");
+    const Lmid_t lmid = have_dlopen_lmid ? dlopen_lmid : LM_ID_NEWLM;
+    handle = dlmopen(lmid, libname, dlopen_flags);
+    break;
+  case dlopen_mode_dlopen:
+    if (verbose)
+      fprintf(stderr, "[MPItrampoline] Calling dlopen\n");
+    handle = dlopen(libname, dlopen_flags | RTLD_LOCAL | RTLD_DEEPBIND);
+    break;
+  default:
+    assert(0);
+  }
+
+#else
+#error "Unsupported operating system"
+#endif
+
+  if (!handle) {
+    fprintf(stderr, "Could not dlopen library \"%s\"\n", libname);
+    const char *const error = dlerror();
+    if (error)
+      fprintf(stderr, "dlerror: %s\n", error);
+    exit(1);
+  }
+
+#ifdef __linux__
+  Lmid_t lmid;
+  const int ierr = dlinfo(handle, RTLD_DI_LMID, &lmid);
+  if (ierr) {
+    fprintf(stderr, "Could not determine link map id\n");
+    const char *const error = dlerror();
+    if (error)
+      fprintf(stderr, "dlerror: %s\n", error);
+    exit(1);
+  }
+
+  if (have_dlopen_lmid) {
+    assert(dlopen_lmid == lmid);
+  } else {
+    dlopen_lmid = lmid;
+    have_dlopen_lmid = true;
+  }
+#endif
+
+  return handle;
+}
+
 static void *get_symbol(void *handle, const char *name) {
   void *ptr = dlsym(handle, name);
   if (!ptr) {
@@ -65,13 +202,51 @@ static void *get_symbol(void *handle, const char *name) {
 #endif
 static void __attribute__((__constructor__ CONSTRUCTOR_PRIORITY))
 init_mpitrampoline() {
-  const bool verbose = getenv("MPITRAMPOLINE_VERBOSE");
+  set_verbose();
+
   if (verbose) {
     fprintf(stderr, "[MPItrampoline] This is MPItrampoline %d.%d.%d\n",
             MPITRAMPOLINE_VERSION_MAJOR, MPITRAMPOLINE_VERSION_MINOR,
             MPITRAMPOLINE_VERSION_PATCH);
     fprintf(stderr, "[MPItrampoline] Requiring MPI ABI version %d.%d.%d\n",
             MPIABI_VERSION_MAJOR, MPIABI_VERSION_MINOR, MPIABI_VERSION_PATCH);
+  }
+
+  // On Linux (with glibc), we use `dlmopen` or `RTLD_DEEPBIND` to
+  // ensure that the loaded `mpiwrapper.so` looks for its MPI symbols
+  // only in its dependencies (the "real" MPI library), and not in
+  // MPItrampoline. If this happens, the resulting recursion leads to
+  // a stack overflow and thus a segfault.
+  //
+  // Using `dlmopen` is more reliable than using `dlopen` with
+  // `RTLD_DEEPBIND`. Unfortunately, this doesn't always work -- it
+  // doesn't always load all the (transitive) dependencies of the
+  // loaded library. I assume this is due to configuration errors in
+  // the way `libwrapper.so` or its dependencies (the "real" MPI
+  // libraries) are built.
+  //
+  // The respective mechanism on macOS is to use two-level namespaces
+  // for the plugin `mpiwrapper.so`. This is a link-time option for
+  // `mpiwrapper.so`, and is also the default (`-twolevel_namespace`).
+  // If `libmpiwrapper.so` is accidentally built with the linker
+  // option `-flat_namespace`, things break as described above.
+
+  set_dlopen_mode();
+  set_dlopen_binding();
+
+  const char *const preload_str = getenv("MPITRAMPOLINE_PRELOAD");
+  if (preload_str) {
+    char *const preload = strdup(preload_str);
+    const char *const delim = ":";
+    char *saveptr;
+    const char *libname = strtok_r(preload, delim, &saveptr);
+    while (libname) {
+      if (verbose)
+        fprintf(stderr, "[MPItrampoline] Preloading library \"%s\"\n", libname);
+      load_library(libname);
+      libname = strtok_r(NULL, delim, &saveptr);
+    }
+    free(preload);
   }
 
   const char *const libname = getenv("MPITRAMPOLINE_LIB");
@@ -91,120 +266,7 @@ init_mpitrampoline() {
     fprintf(stderr, "[MPItrampoline] Using MPIwrapper library \"%s\"\n",
             libname);
 
-  // On Linux (with glibc), we use `dlmopen` or `RTLD_DEEPBIND` to
-  // ensure that the loaded `mpiwrapper.so` looks for its MPI
-  // symbols only in its dependencies (the "real" MPI library), and
-  // not in MPItrampoline. If this happens, the resulting recursion
-  // leads to a stack overflow and thus a segfault.
-  //
-  // The respective mechanism on macOS is to use two-level namespaces
-  // for the plugin `mpiwrapper.so`. This is a link-time option for
-  // `mpiwrapper.so`, and is also the default (`-twolevel_namespace`).
-  // If `libmpiwrapper.so` is accidentally built with the linker
-  // option `-flat_namespace`, things break as described above.
-
-  const char *const dlopen_binding_str = getenv("MPITRAMPOLINE_DLOPEN_BINDING");
-  enum dlopen_binding_t { dlopen_binding_lazy, dlopen_binding_now };
-  enum dlopen_binding_t dlopen_binding;
-  if (dlopen_binding_str == NULL || strcmp(dlopen_binding_str, "lazy") == 0) {
-    dlopen_binding = dlopen_binding_lazy;
-  } else if (strcmp(dlopen_binding_str, "now") == 0) {
-    dlopen_binding = dlopen_binding_now;
-  } else {
-    fprintf(stderr,
-            "The environment variable MPITRAMPOLINE_DLOPEN_BINDING is set to "
-            "\"%s\".\n"
-            "Only the values \"lazy\" (default) and \"now\" are allowed.",
-            dlopen_binding_str);
-    exit(1);
-  }
-
-  int dlopen_flags;
-  switch (dlopen_binding) {
-  case dlopen_binding_lazy:
-    if (verbose)
-      fprintf(stderr, "Using RTLD_LAZY\n");
-    dlopen_flags = RTLD_LAZY;
-    break;
-  case dlopen_binding_now:
-    if (verbose)
-      fprintf(stderr, "Using RTLD_NOW\n");
-    dlopen_flags = RTLD_NOW;
-    break;
-  default:
-    assert(0);
-  }
-
-  void *handle = NULL;
-
-#ifdef __APPLE__
-
-  handle = dlopen(libname, dlopen_flags | RTLD_LOCAL | RTLD_FIRST);
-
-#elif __linux__
-
-  const char *const dlopen_mode_str = getenv("MPITRAMPOLINE_DLOPEN_MODE");
-  enum dlopen_mode_t { dlopen_mode_dlmopen, dlopen_mode_dlopen };
-  enum dlopen_mode_t dlopen_mode;
-  if (dlopen_mode_str == NULL || strcmp(dlopen_mode_str, "dlmopen") == 0) {
-    dlopen_mode = dlopen_mode_dlmopen;
-  } else if (strcmp(dlopen_mode_str, "dlopen") == 0) {
-    dlopen_mode = dlopen_mode_dlopen;
-  } else {
-    fprintf(
-        stderr,
-        "The environment variable MPITRAMPOLINE_DLOPEN_MODE is set to \"%s\".\n"
-        "Only the values \"dlmopen\" (default) and \"dlopen\" are allowed.",
-        dlopen_mode_str);
-    exit(1);
-  }
-
-  switch (dlopen_mode) {
-  case dlopen_mode_dlmopen:
-    // Using `dlmopen` is more reliable than using `dlopen` with
-    // `RTLD_DEEPBIND`. Unfortunately, this doesn't always work -- it
-    // doesn't always load all the (transitive) dependencies of the
-    // loaded library. I assume this is due to configuration errors in
-    // the way `libwrapper.so` or its dependencies (the "real" MPI
-    // libraries) are built.
-    if (verbose)
-      fprintf(stderr, "Calling dlmopen\n");
-    handle = dlmopen(LM_ID_NEWLM, libname, dlopen_flags);
-    break;
-  case dlopen_mode_dlopen:
-    if (verbose)
-      fprintf(stderr, "Calling dlopen\n");
-    handle = dlopen(libname, dlopen_flags | RTLD_LOCAL | RTLD_DEEPBIND);
-    break;
-  default:
-    assert(0);
-  }
-
-#else
-#error "Unsupported operating system"
-#endif
-
-  if (!handle) {
-    fprintf(stderr, "Could not dlopen MPI wrapper library \"%s\"\n", libname);
-    const char *const error = dlerror();
-    if (error)
-      fprintf(stderr, "dlerror: %s\n", error);
-    exit(1);
-  }
-
-#ifdef __linux__
-  {
-    Lmid_t lmid;
-    const int ierr = dlinfo(handle, RTLD_DI_LMID, &lmid);
-    if (ierr) {
-      fprintf(stderr, "Could not determine link map id\n");
-      const char *const error = dlerror();
-      if (error)
-        fprintf(stderr, "dlerror: %s\n", error);
-      exit(1);
-    }
-  }
-#endif
+  void *handle = load_library(libname);
 
   mpiwrapper_version_major =
       *(int const *)get_symbol(handle, "mpiwrapper_version_major");
