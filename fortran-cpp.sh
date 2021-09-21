52e5030842aa6856b3e6406fe0802a73b1d56092
Erik Schnetter <schnetter@gmail.com>
2021-09-21 10:20:58 -0400

    Remove unused file

--- fortran-cpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cpp $1 |
-    sed -e 's/ *## *//g' |
-    grep -v '^//' |
-    sed -e 's/\[n\]/\n      /g' |
-    sed -e 's/\[c\]/                                                                        \&\n     \&/g' |
-    sed -e 's/\(^.\{72\}\) *\&$/\1\&/g' |
-    grep -v '^#' |
-    grep -v '^$' >$2
