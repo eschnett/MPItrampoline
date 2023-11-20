16ececa69052c78e037cb085fb1805a39421219a
Erik Schnetter <schnetter@gmail.com>
2023-11-20 16:58:51 -0500

    CI: Install OpenMPI 5.0

--- .github/workflows/install-openmpi.sh
+++ .github/workflows/install-openmpi.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.0.tar.bz2
-tar xzf openmpi-5.0.0.tar.bz2
+tar xjf openmpi-5.0.0.tar.bz2
 cd openmpi-5.0.0
 ./configure
 make -j$(nproc)
